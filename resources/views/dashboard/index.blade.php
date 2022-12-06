603a8ff440e4c4ba64cce7731ea9f25ea615824b
fari72 <fariaffrizal@gmail.com>
2022-12-06 08:32:26 +0700

    menghapus komentar setting

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -57,21 +57,6 @@ Dashboard
                 </div>
             </div>
         </div>
-        {{-- <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-success">
-                    <i class="fas fa-cog"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>Setting</h4>
-                    </div>
-                    <div class="card-body">
-                        -
-                    </div>
-                </div>
-            </div>
-        </div> --}}
     </div>
 </div>
 @endsection
