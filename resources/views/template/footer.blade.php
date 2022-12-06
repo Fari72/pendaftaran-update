84988517ccce3246652df7e0b3e704c18bbd4eb0
fari72 <fariaffrizal@gmail.com>
2022-12-06 08:32:51 +0700

    mengedit tahun footer

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-      Copyright &copy; 2022 <div class="bullet"></div> Design By <a href="https://www.youtube.com/">AIZAN</a>
+      Copyright &copy; 222 <div class="bullet"></div> Design By <a href="https://www.youtube.com/">AIZAN</a>
     </div>
     <div class="footer-right">
       
