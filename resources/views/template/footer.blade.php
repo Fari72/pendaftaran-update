68e4e38db7f555202487f4af5df7d1c484b1318a
fari72 <fariaffrizal@gmail.com>
2022-12-06 08:46:04 +0700

    menambahkan logo di footer

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,8 +1,7 @@
 <footer class="main-footer">
     <div class="footer-left">
-      Copyright &copy; 222 <div class="bullet"></div> Design By <a href="https://www.youtube.com/">AIZAN</a>
+      Copyright &copy; 222 <div class="bullet"></div> Design By <a href="https://www.youtube.com/">AIZAN </a><i class="fa-solid fa-mug-hot"></i>
     </div>
     <div class="footer-right">
-      
     </div>
   </footer>
